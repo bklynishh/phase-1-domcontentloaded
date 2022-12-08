9f9acd35e7836c2f55997cbf78c5a81c85968724
Hannah Maratea <maratea101@gmail.com>
2022-12-08 11:21:38 -0500

    commit tree 035e2052067e81cd7786c2ee18ddb3dfe074f7ad

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+// Set up DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", (e) => {
+    document.querySelector("#text").innerHTML = "This is really cool!"
+});
